9ae73039f5d5c8daa94d474b566c5a3ceaf4408c
Sneha280564 <sneha.agarwal.pa19@gmail.com>
2024-04-02 02:30:29 +0530

    transformation page

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,13 +1,21 @@
-import React from 'react'
+import Header from '@/components/shared/Header'
+import TransformationForm from '@/components/shared/TransformationForm';
+import { transformationTypes } from '@/constants'
 
 const AddTransformationTypePage
- = () => {
-  return (
-    <div>
-      AddTransformationTypePage
-    </div>
-  )
-}
+  = ({ params: { type } }: SearchParamProps) => {
+    const transformation = transformationTypes[type];
+    return (
+      <>
+        <Header
+          title={transformation.title}
+          subtitle={transformation.subTitle}
+        />
+        <TransformationForm />
+      </>
+
+    )
+  }
 
 export default AddTransformationTypePage
 
